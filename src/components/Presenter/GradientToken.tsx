86832ee6758ff0acc14784400367086e0ffdfa2b
Philipp Siekmann <philipp.siekmann@uxi.de>
2020-04-02 08:44:29 +0200

    fix whitespace in GradientToken.tsx

--- src/components/Presenter/GradientToken.tsx
+++ src/components/Presenter/GradientToken.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import { Box } from '../primitives/Box';
 
 interface Props {
-    gradient: string;
+  gradient: string;
 }
 
 export const GradientToken = (props: Props) => {
-    return <Box style={{ background: props.gradient }} />;
-}
+  return <Box style={{ background: props.gradient }} />;
+};
