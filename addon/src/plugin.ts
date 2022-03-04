045144e7ef1188060d5ee4614f02444301597720
Philipp Siekmann <philipp.siekmann@uxi.de>
2022-03-04 09:44:05 +0100

    separate webpack 4 and 5 plugins

--- addon/src/plugin.ts
+++ addon/src/plugin.ts
@@ -2,34 +2,45 @@ import { readFileSync } from 'fs';
 import glob from 'glob';
 import path from 'path';
 
-export class StorybookDesignTokenPlugin {
+function getTokenFilePaths(compiler: any): string[] {
+  return glob.sync(
+    path.join(
+      compiler.context,
+      process.env.DESIGN_TOKEN_GLOB || '**/*.{css,scss,less,svg}'
+    )
+  );
+}
+
+function addFilesToWebpackDeps(compilation: any, files: string[]) {
+  compilation.fileDependencies = [...compilation.fileDependencies, ...files];
+}
+
+function generateTokenFilesJsonString(files: string[]): string {
+  const tokenFiles = files
+    .map((path) => ({
+      filename: path,
+      content: readFileSync(path, 'utf-8')
+    }))
+    .filter(
+      (file) =>
+        file.content.includes('@tokens') || file.filename.endsWith('.svg')
+    );
+
+  // TODO: move parsing from React components to here
+
+  return JSON.stringify(tokenFiles);
+}
+
+export class StorybookDesignTokenPluginWebpack4 {
   public apply(compiler: any) {
     compiler.hooks.emit.tap(
       'StorybookDesignTokenPlugin',
       (compilation: any) => {
-        const files = glob.sync(
-          path.join(
-            compiler.context,
-            process.env.DESIGN_TOKEN_GLOB || '**/*.{css,scss,less,svg}'
-          )
-        );
+        const files = getTokenFilePaths(compiler);
 
-        compilation.fileDependencies = [
-          ...compilation.fileDependencies,
-          ...files
-        ];
+        addFilesToWebpackDeps(compilation, files);
 
-        const tokenFiles = files
-          .map((path) => ({
-            filename: path,
-            content: readFileSync(path, 'utf-8')
-          }))
-          .filter(
-            (file) =>
-              file.content.includes('@tokens') || file.filename.endsWith('.svg')
-          );
-
-        const sourceString = JSON.stringify(tokenFiles);
+        const sourceString = generateTokenFilesJsonString(files);
 
         compilation.assets['design-tokens.source.json'] = {
           source: () => {
@@ -43,5 +54,48 @@ export class StorybookDesignTokenPlugin {
         return true;
       }
     );
+  }
+}
+
+export class StorybookDesignTokenPlugin {
+  public apply(compiler: any) {
+    compiler.hooks.initialize.tap('StorybookDesignTokenPlugin', () => {
+      const files = getTokenFilePaths(compiler);
+
+      compiler.hooks.emit.tap(
+        'StorybookDesignTokenPlugin',
+        (compilation: any) => {
+          addFilesToWebpackDeps(compilation, files);
+        }
+      );
+
+      compiler.hooks.thisCompilation.tap(
+        'StorybookDesignTokenPlugin',
+        (compilation: any) => {
+          compilation.hooks.processAssets.tapAsync(
+            {
+              name: 'HtmlWebpackPlugin',
+              stage:
+                compiler.webpack.Compilation
+                  .PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE
+            },
+            (compilationAssets: any, callback: any) => {
+              const sourceString = generateTokenFilesJsonString(files);
+
+              compilationAssets['design-tokens.source.json'] = {
+                source: () => {
+                  return sourceString;
+                },
+                size: () => {
+                  return sourceString.length;
+                }
+              };
+
+              callback();
+            }
+          );
+        }
+      );
+    });
   }
 }
--- addon/src/preset.ts
+++ addon/src/preset.ts
@@ -1,11 +1,17 @@
-import { StorybookDesignTokenPlugin } from './plugin';
+import { StorybookDesignTokenPlugin, StorybookDesignTokenPluginWebpack4 } from './plugin';
 
 export function managerEntries(entry = []) {
   return [...entry, require.resolve('./register')];
 }
 
-export async function webpackFinal(config: any) {
-  config.plugins.push(new StorybookDesignTokenPlugin());
+export async function webpackFinal(config: any, { presets }: any) {
+  const version = await presets.apply('webpackVersion');
+
+  if (version >= 5) {
+    config.plugins.push(new StorybookDesignTokenPlugin());
+  } else {
+    config.plugins.push(new StorybookDesignTokenPluginWebpack4());
+  }
 
   return config;
 }
