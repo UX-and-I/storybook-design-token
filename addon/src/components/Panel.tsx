f485814cbc0afef23244ecab6f7ffda9a1dded43
Cherkashin Alexander <cherkalexander@gmail.com>
2022-04-18 21:50:02 +0300

    initial search implementation

--- addon/src/components/Panel.tsx
+++ addon/src/components/Panel.tsx
@@ -1,42 +1,47 @@
 import React from 'react';
 
 import { useParameter } from '@storybook/api';
-import { ActionBar, ScrollArea, Tabs } from '@storybook/components';
+import { ActionBar, ScrollArea, Tabs, Form, Icons } from '@storybook/components';
 
 import { useTokenTabs } from '../hooks/useTokenTabs';
 import { Config } from '../types/config.types';
 import { TokenCards } from './TokenCards';
 import { TokenTable } from './TokenTable';
+import { useState } from 'react';
+import { Input } from './Input';
+import { useDebounce } from '../hooks/useDebounce';
 
 export const Panel = () => {
   const config = useParameter<Config>('designToken');
+  const [searchText, setSearchText] = useState('');
+  const debouncedSearchText = useDebounce(searchText, 250);
 
-  const {
-    activeCategory,
-    cardView,
-    setActiveCategory,
-    setCardView,
-    styleInjections,
-    tabs
-  } = useTokenTabs(config);
+  const { activeCategory, cardView, setActiveCategory, setCardView, styleInjections, tabs } = useTokenTabs(config);
 
   return (
     <>
       <style>{styleInjections}</style>
 
       <ScrollArea vertical horizontal>
-        <Tabs
-          actions={{ onSelect: (id) => setActiveCategory(id) }}
-          selected={activeCategory}
-        >
-          {tabs.map((tab) => (
-            <div key={tab.label} id={tab.label} title={tab.label}>
-              <div>
-                {cardView && <TokenCards categories={tab.categories} />}
-                {!cardView && <TokenTable categories={tab.categories} />}
+        <Input value={searchText} onChange={e => setSearchText(e.target.value)} />
+        <Tabs actions={{ onSelect: id => setActiveCategory(id) }} selected={activeCategory}>
+          {tabs.map(tab => {
+            const categories = debouncedSearchText
+              ? tab.categories.map(item => ({
+                  ...item,
+                  tokens: item.tokens.filter(token => token.name.includes(debouncedSearchText)),
+                }))
+              : tab.categories;
+
+            return (
+              <div key={tab.label} id={tab.label} title={tab.label}>
+                <div>
+                  {cardView && <TokenCards categories={categories} />}
+                  {!cardView && <TokenTable categories={categories} />}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </Tabs>
       </ScrollArea>
 
@@ -47,8 +52,8 @@ export const Panel = () => {
             onClick: () => {
               setCardView(!cardView);
             },
-            title: cardView ? 'Table View' : 'Card View'
-          }
+            title: cardView ? 'Table View' : 'Card View',
+          },
         ]}
       />
     </>
