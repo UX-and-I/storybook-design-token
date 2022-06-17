2cfcc603bf1cdff924d6b2e7dc4821f040e2edd0
Michał Malik <michalmmalik@gmail.com>
2022-06-17 13:17:12 +0200

    Allow to preserve CSS vars

--- addon/src/preset.ts
+++ addon/src/preset.ts
@@ -1,16 +1,21 @@
 import { StorybookDesignTokenPlugin, StorybookDesignTokenPluginWebpack4 } from './plugin';
 
+type Options = {
+  presets: any;
+  preserveCSSVars?: boolean;
+}
+
 export function managerEntries(entry = []) {
   return [...entry, require.resolve('./register')];
 }
 
-export async function webpackFinal(config: any, { presets }: any) {
+export async function webpackFinal(config: any, { presets, preserveCSSVars }: Options) {
   const version = await presets.apply('webpackVersion');
 
   if (version >= 5) {
-    config.plugins.push(new StorybookDesignTokenPlugin());
+    config.plugins.push(new StorybookDesignTokenPlugin(preserveCSSVars));
   } else {
-    config.plugins.push(new StorybookDesignTokenPluginWebpack4());
+    config.plugins.push(new StorybookDesignTokenPluginWebpack4(preserveCSSVars));
   }
 
   return config;
