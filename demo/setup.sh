4a3c05d6dab769f321f2e28f4016b58c8cfdc090
Philipp Siekmann <philippsiekmann@me.com>
2023-03-21 15:00:31 +0100

    chore(demo): add setup script + demo code

---
+++ demo/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+echo "${BOLD}Setting up storybook-design-token addon dev environment …${NORMAL}"
+
+cd $SCRIPT_DIR/../addon
+yarn > /dev/null 2>&1
+yarn link --silent > /dev/null 2>&1
+
+cd $SCRIPT_DIR
+yarn > /dev/null 2>&1
+yarn link --silent storybook-design-token
+
+cd $SCRIPT_DIR/node_modules/react
+yarn link --silent > /dev/null 2>&1
+
+cd $SCRIPT_DIR/node_modules/react-dom
+yarn link --silent > /dev/null 2>&1
+
+cd $SCRIPT_DIR/../addon
+yarn link --silent react
+yarn link --silent react-dom
+
+echo "${BOLD}Done!${NORMAL}"
--- demo/stories/Button.js
+++ demo/stories/Button.js
@@ -1,11 +1,31 @@
 export const createButton = ({ label, onClick }) => {
+  const container = document.createElement('div');
   const btn = document.createElement('button');
+  const styles = document.createElement('style');
+
+  styles.innerHTML = `
+    :root {
+      --border-radius-s: 4px;
+      --brand: hsl(240, 100%, 50%);
+      --spacing-s: 8px;
+    }
+  `;
 
   btn.type = 'button';
   btn.innerText = label;
+
+  btn.style.backgroundColor = 'var(--brand)';
+  btn.style.border = 'none';
+  btn.style.borderRadius = 'var(--border-radius-s)';
+  btn.style.cursor = 'pointer';
+  btn.style.color = '#fff';
+  btn.style.padding = 'var(--spacing-s)';
+
   btn.addEventListener('click', onClick);
 
   btn.className = ['storybook-button'];
 
-  return btn;
+  container.append(styles, btn);
+
+  return container;
 };
